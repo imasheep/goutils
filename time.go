b452b4c116fbf769f8bbf85bb714a096c609536c
imasheep <18622513004@163.com>
2018-01-07 20:21:24 +0800

    update time add getDurationDesc

--- time.go
+++ time.go
@@ -1,6 +1,9 @@
 package goutils
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func GetTimeStamp() int64 {
 
@@ -11,5 +14,26 @@ func GetTimeStamp() int64 {
 func GetTimeFormat(timeFormat string) (currentTime string) {
 
 	return time.Now().Format(timeFormat)
+
+}
+
+func GetDurationDesc(durationTime time.Duration) (desc string) {
+
+	duration := durationTime.Seconds()
+
+	switch {
+	case duration <= 60*10:
+		desc = "刚刚"
+	case duration <= 60*60:
+		desc = fmt.Sprintf("%d分钟前", int(duration)/60)
+	case duration <= 60*60*24:
+		desc = fmt.Sprintf("%d小时前", int(duration)/60/60)
+	case duration <= 60*60*24*30:
+		desc = fmt.Sprintf("%d天前", int(duration)/3600/24)
+	default:
+		desc = fmt.Sprintf("%d月前", int(duration)/3600/24/30)
+	}
+
+	return desc
 
 }
