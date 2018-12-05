d48b41ca82a0bb845e00858d3e2bd7729419fb6c
xuyang <xuyang@yunshuxie.com>
2018-12-05 16:18:56 +0800

    xuyang update

--- ldap/ldap.go
+++ ldap/ldap.go
@@ -2,6 +2,7 @@
 package ldap
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/toolkits/ldap"
@@ -16,23 +17,64 @@ type LdapInstance struct {
 	Attributes   []string
 }
 
-func (this *LdapInstance) Search(user string, password string) (ok bool, attrs map[string]string, err error) {
+var Ldap *LdapInstance
 
-	attrs = make(map[string]string)
-	filter := "(" + this.UserField + "=" + user + ")"
+func Init(ldapInstance LdapInstance) {
+	Ldap = &ldapInstance
+	return
+}
+
+func New() (l *LdapInstance) {
+	l = Ldap
+	return
+}
+
+func (this *LdapInstance) bind() (conn *ldap.Conn, err error) {
+
+	fmt.Println(this.Addr)
+	conn, err = ldap.Dial("tcp", this.Addr)
+	if err != nil {
+		return
+	}
+
+	err = conn.Bind(this.BindDn, this.BindPassword)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (this *LdapInstance) Auth(user string, password string) (ok bool, err error) {
+	conn, err := this.bind()
+	if err != nil {
+		return false, err
+	}
+	userAttrs, err := this.Get(user)
+	if err != nil {
+		return false, err
+	}
+	err = conn.Bind(userAttrs["dn"], password)
+	if err != nil {
+		return false, err
+	}
+	return true, err
+}
+
+func (this *LdapInstance) Get(user string) (userAttrs map[string]string, err error) {
+
+	userAttrs = make(map[string]string)
+
 	conn, err := ldap.Dial("tcp", this.Addr)
-
 	if err != nil {
-		return false, attrs, fmt.Errorf("dial ldap fail: %s", err.Error())
+		return
 	}
-	defer conn.Close()
 
-	if this.BindDn != "" {
-		err = conn.Bind(this.BindDn, this.BindPassword)
-	}
+	err = conn.Bind(this.BindDn, this.BindPassword)
 	if err != nil {
-		return false, attrs, fmt.Errorf("ldap Bind fail: %s", err.Error())
+		return
 	}
+
+	filter := "(" + this.UserField + "=" + user + ")"
 
 	search := ldap.NewSearchRequest(
 		this.BaseDn,
@@ -41,24 +83,17 @@ func (this *LdapInstance) Search(user string, password string) (ok bool, attrs m
 		nil,
 		nil)
 
-	sr, err := conn.Search(search)
+	searchResult, err := conn.Search(search)
 	if err != nil {
-		return false, attrs, fmt.Errorf("ldap search fail: %s", err.Error())
+		return userAttrs, fmt.Errorf("ldap search fail: %s", err.Error())
 	}
 
-	defer func() {
-		if err := recover(); err != nil {
-			ok = false
-		}
-	}()
-	err = conn.Bind(sr.Entries[0].DN, password)
-	if err != nil {
-		return false, attrs, err
+	if len(searchResult.Entries) == 0 {
+		return nil, errors.New("No Such User")
 	}
-
 	for _, key := range this.Attributes {
-		attrs[key] = sr.Entries[0].GetAttributeValue(key)
+		userAttrs[key] = searchResult.Entries[0].GetAttributeValue(key)
 	}
-
-	return true, attrs, err
+	userAttrs["dn"] = searchResult.Entries[0].DN
+	return
 }
